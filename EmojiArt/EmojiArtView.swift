52b55e572a5359cc1bcfc489cfa26c8200ad61b7
Ashish Pal <2001ashish@gmail.com>
2019-12-08 23:46:12 +0530

    Emoji Art Project Created

---
+++ EmojiArt/EmojiArtView.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by Ashish Pal on 08/12/19.
+//  Copyright © 2019 Ashish Pal. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EmojiArt/EmojiArtViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by Ashish Pal on 08/12/19.
+//  Copyright © 2019 Ashish Pal. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
