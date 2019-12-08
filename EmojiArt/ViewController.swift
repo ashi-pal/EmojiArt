2bbc8f524f186ea4e6b7781bcd1499c008854e9c
Ashish Pal <2001ashish@gmail.com>
2019-12-08 18:21:19 +0530

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Ashish Pal on 08/12/19.
+//  Copyright © 2019 Ashish Pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
